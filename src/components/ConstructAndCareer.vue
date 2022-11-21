13a3cb73cc0dec291fb573e62050b8a03d9644fb
karfaisarra <karfaisarra@gmail.com>
2022-11-21 18:51:33 +0100

    add construct and career section

--- src/components/ConstructAndCareer.vue
+++ src/components/ConstructAndCareer.vue
@@ -4,8 +4,216 @@ export default {
 }
 </script>
 <template>
+    <section class="construct_and_career">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-6 text-center ">
+                    <img width="300" src="../assets/img/about-us-03-image-01.png" alt="">
+                </div>
+                <div class="col-5">
+                    <p class="subtitle">Deram with Maxcoach</p>
+                    <h2>Construct A <span>Stunning</span> Career Perspective</h2>
+                    <div class="blocks">
+                        <div class="block shadow-lg mb-3 bg-body rounded">
+                            <div class="show">
+                                <div class="text d-flex align-items-center justify-content-between">
+                                    <p>Multiple Platform Supported for teaching & Studying</p>
+                                    <div class="icon">
+                                        <svg class="first" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                            viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                            xml:space="preserve">
+                                            <g>
+                                                <path d="M368.5,240H272v-96.5c0-8.8-7.2-16-16-16c-8.8,0-16,7.2-16,16V240h-96.5c-8.8,0-16,7.2-16,16c0,4.4,1.8,8.4,4.7,11.3
+		                                    c2.9,2.9,6.9,4.7,11.3,4.7H240v96.5c0,4.4,1.8,8.4,4.7,11.3c2.9,2.9,6.9,4.7,11.3,4.7c8.8,0,16-7.2,16-16V272h96.5
+		                                    c8.8,0,16-7.2,16-16C384.5,247.2,377.3,240,368.5,240z" fill="#fff" />
+                                            </g>
+                                        </svg>
+                                        <svg class="last" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+                                            viewBox="0 0 129 129" enable-background="new 0 0 129 129" width="16px"
+                                            height="16px">
+                                            <g>
+                                                <path
+                                                    d="M7.6,121.4c0.8,0.8,1.8,1.2,2.9,1.2s2.1-0.4,2.9-1.2l51.1-51.1l51.1,51.1c0.8,0.8,1.8,1.2,2.9,1.2c1,0,2.1-0.4,2.9-1.2   c1.6-1.6,1.6-4.2,0-5.8L70.3,64.5l51.1-51.1c1.6-1.6,1.6-4.2,0-5.8s-4.2-1.6-5.8,0L64.5,58.7L13.4,7.6C11.8,6,9.2,6,7.6,7.6   s-1.6,4.2,0,5.8l51.1,51.1L7.6,115.6C6,117.2,6,119.8,7.6,121.4z"
+                                                    fill="#FFFFFF" />
+                                            </g>
+                                        </svg>
 
+                                    </div>
+                                </div>
+                                <div class="hidden">
+                                    <p>MaxCoach supports the act of teaching and learning on multiple platforms like
+                                        online
+                                        or offline via material download. We know things aren't supposed to be devoured
+                                        in a
+                                        short time, you can always access our knowledge base from any device.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="block shadow-lg mb-3 bg-body rounded">
+                            <div class="show">
+                                <div class="text d-flex align-items-center justify-content-between">
+                                    <p>Multiple Course Participation at the Same Time</p>
+                                    <div class="icon">
+                                        <svg class="first" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                            viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                            xml:space="preserve">
+                                            <g>
+                                                <path d="M368.5,240H272v-96.5c0-8.8-7.2-16-16-16c-8.8,0-16,7.2-16,16V240h-96.5c-8.8,0-16,7.2-16,16c0,4.4,1.8,8.4,4.7,11.3
+		                                    c2.9,2.9,6.9,4.7,11.3,4.7H240v96.5c0,4.4,1.8,8.4,4.7,11.3c2.9,2.9,6.9,4.7,11.3,4.7c8.8,0,16-7.2,16-16V272h96.5
+		                                    c8.8,0,16-7.2,16-16C384.5,247.2,377.3,240,368.5,240z" fill="#fff" />
+                                            </g>
+                                        </svg>
+                                        <svg class="last" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+                                            viewBox="0 0 129 129" enable-background="new 0 0 129 129" width="16px"
+                                            height="16px">
+                                            <g>
+                                                <path
+                                                    d="M7.6,121.4c0.8,0.8,1.8,1.2,2.9,1.2s2.1-0.4,2.9-1.2l51.1-51.1l51.1,51.1c0.8,0.8,1.8,1.2,2.9,1.2c1,0,2.1-0.4,2.9-1.2   c1.6-1.6,1.6-4.2,0-5.8L70.3,64.5l51.1-51.1c1.6-1.6,1.6-4.2,0-5.8s-4.2-1.6-5.8,0L64.5,58.7L13.4,7.6C11.8,6,9.2,6,7.6,7.6   s-1.6,4.2,0,5.8l51.1,51.1L7.6,115.6C6,117.2,6,119.8,7.6,121.4z"
+                                                    fill="#FFFFFF" />
+                                            </g>
+                                        </svg>
+
+                                    </div>
+                                </div>
+                                <div class="hidden">
+                                    <p>MaxCoach supports the act of teaching and learning on multiple platforms like
+                                        online
+                                        or offline via material download. We know things aren't supposed to be devoured
+                                        in a
+                                        short time, you can always access our knowledge base from any device.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="block shadow-lg mb-3 bg-body rounded">
+                            <div class="show">
+                                <div class="text d-flex align-items-center justify-content-between">
+                                    <p>Track Study Progress & Deliver Prompt Feedback</p>
+                                    <div class="icon">
+                                        <svg class="first" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                            viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                            xml:space="preserve">
+                                            <g>
+                                                <path d="M368.5,240H272v-96.5c0-8.8-7.2-16-16-16c-8.8,0-16,7.2-16,16V240h-96.5c-8.8,0-16,7.2-16,16c0,4.4,1.8,8.4,4.7,11.3
+		                                    c2.9,2.9,6.9,4.7,11.3,4.7H240v96.5c0,4.4,1.8,8.4,4.7,11.3c2.9,2.9,6.9,4.7,11.3,4.7c8.8,0,16-7.2,16-16V272h96.5
+		                                    c8.8,0,16-7.2,16-16C384.5,247.2,377.3,240,368.5,240z" fill="#fff" />
+                                            </g>
+                                        </svg>
+                                        <svg class="last" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+                                            viewBox="0 0 129 129" enable-background="new 0 0 129 129" width="16px"
+                                            height="16px">
+                                            <g>
+                                                <path
+                                                    d="M7.6,121.4c0.8,0.8,1.8,1.2,2.9,1.2s2.1-0.4,2.9-1.2l51.1-51.1l51.1,51.1c0.8,0.8,1.8,1.2,2.9,1.2c1,0,2.1-0.4,2.9-1.2   c1.6-1.6,1.6-4.2,0-5.8L70.3,64.5l51.1-51.1c1.6-1.6,1.6-4.2,0-5.8s-4.2-1.6-5.8,0L64.5,58.7L13.4,7.6C11.8,6,9.2,6,7.6,7.6   s-1.6,4.2,0,5.8l51.1,51.1L7.6,115.6C6,117.2,6,119.8,7.6,121.4z"
+                                                    fill="#FFFFFF" />
+                                            </g>
+                                        </svg>
+
+                                    </div>
+                                </div>
+                                <div class="hidden">
+                                    <p>MaxCoach supports the act of teaching and learning on multiple platforms like
+                                        online
+                                        or offline via material download. We know things aren't supposed to be devoured
+                                        in a
+                                        short time, you can always access our knowledge base from any device.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 <style lang="scss" scoped>
+@use '../assets/scss/variables' as *;
 
+.construct_and_career {
+    .col-5 {
+        .subtitle {
+            color: $md-dark-gray;
+            text-transform: uppercase;
+            font-size: 13px;
+            font-weight: 500;
+        }
+
+        h2 {
+            padding-bottom: 1.5rem;
+
+            span {
+                color: $md-success;
+                font-weight: 350;
+            }
+        }
+    }
+
+    .col-6 {
+        background-image: url(../assets/img/underlay-shape-lilla.svg);
+        background-repeat: no-repeat;
+        background-position: left;
+        background-size: contain;
+    }
+
+    .block {
+
+        .show {
+            .hidden {
+                padding: 1rem;
+                display: none;
+            }
+
+            .icon .last {
+                width: 20px;
+                padding: 0.2rem;
+                display: none;
+            }
+
+            .text {
+                padding: 1rem;
+
+                svg {
+                    background-color: $md-dark-gray;
+                    border-radius: 50%;
+                    width: 20px;
+                    padding: 0.2rem;
+                }
+            }
+
+            &:hover {
+                width: 100%;
+                background-color: $md-soft-primary;
+                color: $md-light;
+                border-radius: 4px;
+                cursor: pointer;
+
+                .hidden {
+                    display: block;
+                    background-color: $md-light;
+                    color: $md-dark-gray;
+                }
+
+                .icon .first {
+                    display: none;
+                }
+
+                .icon .last {
+                    display: block;
+                }
+            }
+
+        }
+
+
+
+    }
+}
 </style>
