20d5b8e9e4a2d3b6f89d1637d1003abcc7de8035
karfaisarra <karfaisarra@gmail.com>
2022-11-22 17:24:12 +0100

    add events section

--- src/components/EventsSection.vue
+++ src/components/EventsSection.vue
@@ -4,8 +4,241 @@ export default {
 }
 </script>
 <template>
-
+    <section class="events">
+        <div class="container my-5">
+            <p class="subtitle">get in contact now</p>
+            <h2>Upcoming <span>Events</span></h2>
+            <div class="row row-cols-lg-2 g-4">
+                <div class="col">
+                    <div class="card p-4 border-0">
+                        <div class="d-flex justify-content-between">
+                            <div class="left">
+                                <p class="country d-flex align-items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                                        class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    </svg>
+                                    <span>Texas, US</span>
+                                </p>
+                                <h5>Storytelling Workshop</h5>
+                            </div>
+                            <div class="right text-center">
+                                <h2>22</h2>
+                                <h6>NOV</h6>
+                                <a href="#" class="btn my_btn">Get ticket</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card p-4 border-0">
+                        <div class="d-flex justify-content-between ">
+                            <div class="left">
+                                <p class="country d-flex align-items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                                        class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    </svg>
+                                    <span>New York, US</span>
+                                </p>
+                                <h5>Painting Art Contest 2020</h5>
+                            </div>
+                            <div class="right text-center">
+                                <h2>10</h2>
+                                <h6>OCT</h6>
+                                <a href="#" class="btn my_btn">Get ticket</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card p-4 border-0">
+                        <div class="d-flex justify-content-between">
+                            <div class="left">
+                                <p class="country d-flex align-items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                                        class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    </svg>
+                                    <span>Hamburg, Germany</span>
+                                </p>
+                                <h5>International Art Fair 2020</h5>
+                            </div>
+                            <div class="right text-center">
+                                <h2>23</h2>
+                                <h6>NOV</h6>
+                                <a href="#" class="btn my_btn">Get ticket</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card p-4 border-0">
+                        <div class="d-flex justify-content-between">
+                            <div class="left">
+                                <p class="country d-flex align-items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                                        class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    </svg>
+                                    <span>Illinois, US</span>
+                                </p>
+                                <h5>Street Performance: Call for Artist</h5>
+                            </div>
+                            <div class="right text-center">
+                                <h2>15</h2>
+                                <h6>DEC</h6>
+                                <a href="#" class="btn my_btn">Get ticket</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card p-4 border-0">
+                        <div class="d-flex justify-content-between">
+                            <div class="left">
+                                <p class="country d-flex align-items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                                        class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    </svg>
+                                    <span>Illinois, US</span>
+                                </p>
+                                <h5 class="w-75">Consumer Food Safety Education Conference</h5>
+                            </div>
+                            <div class="right text-center">
+                                <h2>22</h2>
+                                <h6>JUL</h6>
+                                <a href="#" class="btn my_btn">Get ticket</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card p-4 border-0">
+                        <div class="d-flex justify-content-between">
+                            <div class="left">
+                                <p class="country d-flex align-items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                                        class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    </svg>
+                                    <span>Dubai, UAE</span>
+                                </p>
+                                <h5 class="w-75">How meditation improve your mental health?</h5>
+                            </div>
+                            <div class="right text-center">
+                                <h2>12</h2>
+                                <h6>AUG</h6>
+                                <a href="#" class="btn my_btn">Get ticket</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="call_to_action">
+                <p>Excited about our envent?
+                    <a href="#">
+                        View all events
+                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor"
+                            class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                        </svg>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </section>
 </template>
 <style lang="scss" scoped>
+@use '../assets/scss/variables' as *;
 
+.events {
+    .subtitle {
+        color: $md-dark-gray;
+        text-transform: uppercase;
+        font-size: 13px;
+        font-weight: 500;
+        text-align: center;
+    }
+
+    h2 {
+        padding-bottom: 1.5rem;
+        text-align: center;
+        margin-bottom: 2rem;
+
+        span {
+            color: $md-success;
+            font-weight: 350;
+        }
+    }
+
+    .card {
+        background-color: #F8F8F8;
+
+        .left {
+            .country {
+
+                //margin-bottom: ;
+                span,
+                svg {
+                    color: $md-dark-gray;
+                }
+
+                span {
+                    font-size: 12px;
+                    font-weight: 500;
+                }
+            }
+
+            h5 {
+                margin-top: -10px;
+            }
+        }
+
+        .right {
+            h2 {
+                color: $md-soft-primary;
+                margin-bottom: -1px;
+            }
+
+            h6 {
+                font-size: 15px;
+            }
+
+            .my_btn {
+                background-color: $md-soft-primary;
+                padding: 0.25rem 2.5rem;
+                color: $md-light;
+                font-weight: 500;
+            }
+        }
+    }
+
+    .call_to_action {
+        margin-top: 5rem;
+        text-align: center;
+        color: $md-dark-gray;
+        font-weight: 500;
+
+        a {
+            color: $md-soft-primary;
+            text-decoration: underline;
+            text-decoration-color: $md-dark-gray;
+        }
+    }
+}
 </style>
