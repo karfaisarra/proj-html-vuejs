67372609408ed527e1afb9f2426264a96365dcc2
karfaisarra <karfaisarra@gmail.com>
2022-11-23 15:11:13 +0100

    add dynamic cards

--- src/components/BuildDream.vue
+++ src/components/BuildDream.vue
@@ -1,6 +1,39 @@
 <script>
 export default {
-    name: 'BuildDream'
+    name: 'BuildDream',
+    data() {
+        return {
+            cards: [
+                {
+                    img: 'home-5-image-box-01',
+                    title: 'Idea Discussion',
+                    description: 'Get teamed up with the specialists who work and teach coding for years at famous universities.',
+
+                },
+                {
+                    img: 'home-5-image-box-02',
+                    title: 'Web Development',
+                    description: 'Learn to start building a webpage from scratch. You decide you own pace, course and speed.'
+                },
+                {
+                    img: 'home-5-image-box-03',
+                    title: 'System Administration',
+                    description: 'Learn are encouraged to study the mechanism and structure of system administration.'
+                },
+                {
+                    img: 'home-5-image-box-04',
+                    title: 'Graphic Design',
+                    description: 'Have a passion for graphic and arts? Show your talents with confidence and self-assertiveness.'
+
+                },
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(url) {
+            return new URL(`../assets/img/${url}.png`, import.meta.url).href
+        }
+    }
 }
 </script>
 <template>
@@ -8,13 +41,29 @@ export default {
         <div class="container">
             <p class="text-uppercase mb-3">Start Learning Coding Languages</p>
             <h2 class="mb-5">Build Your Dream <span>TODAY</span></h2>
-            <div class="row">
-                <div class="col">
-                    <img src="../assets/img/home-5-image-box-01.png" alt="">
+            <div class="row pt-4">
+                <div class="col" v-for="card in cards">
+                    <img class="mb-4" :src="getImageUrl(card.img)" alt="">
                     <div class="details">
-                        <h5>Idea Discussion</h5>
-                        <p>Get teamed up with the specialists who work and teach coding for years at famous
-                            universities.</p>
+                        <h5 class="mb-4">{{ card.title }}</h5>
+                        <p>{{ card.description }}</p>
+                        <h6>
+                            <a href="#" class="btn my_btn">
+                                Start now
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg>
+                            </a>
+                        </h6>
+                    </div>
+                </div>
+                <!--<div class="col">
+                    <img src="" alt="">
+                    <div class="details">
+                        <h5></h5>
+                        <p></p>
                         <h6>
                             <a href="#" class="btn my_btn">
                                 Start now
@@ -28,11 +77,11 @@ export default {
                     </div>
                 </div>
                 <div class="col">
-                    <img src="../assets/img/home-5-image-box-02.png" alt="">
+                    <img src="" alt="">
                     <div class="details">
-                        <h5>Web Development</h5>
+                        <h5></h5>
                         <p>
-                            Learn to start building a webpage from scratch. You decide you own pace, course and speed.
+                            
                         </p>
                         <h6>
                             <a href="#" class="btn my_btn">
@@ -47,11 +96,11 @@ export default {
                     </div>
                 </div>
                 <div class="col">
-                    <img src="../assets/img/home-5-image-box-03.png" alt="">
+                    <img src="" alt="">
                     <div class="details">
-                        <h5>System Administration</h5>
+                        <h5></h5>
                         <p>
-                            Learn are encouraged to study the mechanism and structure of system administration.
+                            
                         </p>
                         <h6>
                             <a href="#" class="btn my_btn">
@@ -64,27 +113,7 @@ export default {
                             </a>
                         </h6>
                     </div>
-                </div>
-                <div class="col">
-                    <img src="../assets/img/home-5-image-box-04.png" alt="">
-                    <div class="details">
-                        <h5>Graphic Design</h5>
-                        <p>
-                            Have a passion for graphic and arts? Show your talents with confidence and
-                            self-assertiveness.
-                        </p>
-                        <h6>
-                            <a href="#" class="btn my_btn">
-                                Start now
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                                </svg>
-                            </a>
-                        </h6>
-                    </div>
-                </div>
+                </div>-->
             </div>
         </div>
     </section>
